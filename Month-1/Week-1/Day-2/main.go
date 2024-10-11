da0efaf764a26af66e5d6346988ef2131658f2e3
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2024-10-11 19:49:14 +0100

    finishing touches

--- Month-1/Week-1/Day-2/main.go
+++ Month-1/Week-1/Day-2/main.go
@@ -13,10 +13,18 @@ func main() {
     input, _ := reader.ReadString('\n') 
 	input = strings.TrimSpace(input)
 
+	found := false
+
 	words := strings.Split(input, " ")
 	for i, word := range words {
 		if word == "Nemo"{
 			fmt.Printf("I found Nemo at %v\n", i)
-		}	
+			found = true
+		}
+		
+	}
+
+	if !found{
+		fmt.Printf("Couldn't find Nemo :(\n")
 	}
 }
