11b04ff8dc1965b0bfd2a2d68c938bbc44ccc356
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2024-10-17 21:22:14 +0100

    creating phone map

---
+++ Month-1/Week-2/Day-8/main.go
@@ -0,0 +1,16 @@
+package main
+
+
+
+func main() {
+	phoneMap := make(map[int][]string)
+	phoneMap[2] = []string{"a", "b", "c"}
+	phoneMap[3] = []string{"d", "e", "f"}
+	phoneMap[4] = []string{"g", "h", "i"}
+	phoneMap[5] = []string{"j", "k", "l"}
+	phoneMap[6] = []string{"m", "n", "o"}
+	phoneMap[7] = []string{"p", "q", "r", "s"}
+	phoneMap[8] = []string{"t", "u", "v"}
+	phoneMap[9] = []string{"w", "x", "y", "z"}
+
+}
